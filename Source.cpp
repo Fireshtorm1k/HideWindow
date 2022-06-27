b13657606b0ad96744a3027b877b247fa59176aa
Fireshtorm1k <84418448+Fireshtorm1k@users.noreply.github.com>
2022-06-28 02:34:14 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,85 @@
+#include <windows.h>
+#include <tlhelp32.h>
+#include <vector>
+#include <wchar.h> 
+#include <stdio.h>
+
+
+
+void getAllProcessIdsByProcNameW(LPCWSTR lpczProcName, std::vector<DWORD>* pidsVec)
+{
+    pidsVec->clear(); 
+    PROCESSENTRY32W procEntry = { sizeof(PROCESSENTRY32W) };
+    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0); 
+    if (Process32FirstW(hSnapshot, &procEntry)) 
+    {
+        while (Process32NextW(hSnapshot, &procEntry)) 
+        {
+            
+            if (wcscmp(procEntry.szExeFile, lpczProcName) == 0)
+            {
+                pidsVec->push_back(procEntry.th32ProcessID);
+            }
+        }
+    }
+
+    CloseHandle(hSnapshot); 
+}
+
+
+void getAllWindowHandlesByPyPidsVec(const std::vector<DWORD>* pidsVec, std::vector<HWND>* hwndVec)
+{
+    hwndVec->clear(); 
+    HWND hCurrentWindow = nullptr; 
+    DWORD dwProcessID = 0; 
+
+    for (size_t i = 0; i < pidsVec->size(); i++) 
+    {
+        while (true)
+        {
+            hCurrentWindow = FindWindowExW(nullptr, hCurrentWindow, nullptr, nullptr); 
+           
+            if (hCurrentWindow == nullptr) 
+            {
+                break; 
+            } и
+            else
+            {
+                GetWindowThreadProcessId(hCurrentWindow, &dwProcessID); 
+                
+                if (dwProcessID == (*pidsVec)[i]) 
+                {
+                    hwndVec->push_back(hCurrentWindow); 
+                }
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    LPCWSTR lpczProcName = L"Telegram.exe";
+    std::vector<DWORD> pidsVec{}; 
+    std::vector<HWND> hwndVec{}; 
+
+    getAllProcessIdsByProcNameW(lpczProcName, &pidsVec);
+    if (!pidsVec.empty()) 
+    {
+        getAllWindowHandlesByPyPidsVec(&pidsVec, &hwndVec);
+        if (!hwndVec.empty())
+        {
+            for (size_t i = 0; i < hwndVec.size(); i++)
+            {
+                
+                wprintf(L"%p\n", hwndVec[i]);
+                ShowWindow(hwndVec[i], SW_SHOW);
+            }
+        }
+
+        else wprintf(L"The PIDs haven't visible (no only message) windows.\n"); 
+    }
+
+    else wprintf(L"There're no processes with specified name.\n"); 
+    return 0;
+}
