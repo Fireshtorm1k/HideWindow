536e1899348e22ad5888e2832adf19bb4775d229
Administrator <admin@example.com>
2024-01-24 16:29:14 +0300

    Automation Test

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
-    std::cout << "Auto Git Hook Success";
+    std::cout << "Auto Git Hook Success"<< '\n';
+	std::cout << "Doing A + B" << '\n';
+	int a = 5;
+	int b = 10;
+	std::cout << a+b;
     return 0;
 }
